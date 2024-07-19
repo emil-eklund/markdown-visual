414408bb7915bdd9b1c8deb6d0431f4ae36cb5dc
Emil Eklund <emil.eklund@aimplan.com>
2024-07-19 11:56:56 +0200

    Implement support for Mermaid diagrams

--- src/visual.ts
+++ src/visual.ts
@@ -9,6 +9,7 @@ import IVisual = powerbi.extensibility.visual.IVisual;
 import { VisualFormattingSettingsModel } from "./settings";
 import { Converter } from "showdown";
 import * as DOMPurify from 'dompurify';
+import mermaid from 'mermaid';
 
 export class Visual implements IVisual {
     private formattingSettings: VisualFormattingSettingsModel;
@@ -27,6 +28,11 @@ export class Visual implements IVisual {
         this.target = container;
         this.converter = new Converter();
         this.converter.setFlavor("github");
+
+        // Initialize Mermaid
+        mermaid.initialize({
+            startOnLoad: false
+        });
     }
 
     public update(options: VisualUpdateOptions) {
@@ -50,6 +56,9 @@ export class Visual implements IVisual {
             // Set the HTML content of the target element.
             this.sanitizeAndSetHtml(html);
 
+            // Render Mermaid diagrams if present
+            this.renderMermaidDiagrams();
+
             // Power BI does not allow visuals to open links directly.
             // We need to use the host to open links.
             const links = this.target.querySelectorAll("a");
@@ -72,12 +81,38 @@ export class Visual implements IVisual {
      * 
      * @param html The HTML content to set.
      */
-    private sanitizeAndSetHtml(html: string) {
+    private sanitizeAndSetHtml(html: string, target = this.target) {
         const sanitizedHtml = DOMPurify.sanitize(html);
 
         // NEVER assign anything to innerHTML that did not come from the sanitizer.
         // eslint-disable-next-line powerbi-visuals/no-inner-outer-html
-        this.target.innerHTML = sanitizedHtml;
+        target.innerHTML = sanitizedHtml;
+    }
+
+    /**
+     * Renders Mermaid diagrams within the target element.
+     */
+    private renderMermaidDiagrams() {
+        const mermaidBlocks = this.target.querySelectorAll('code.language-mermaid');
+
+        mermaidBlocks.forEach((block, index) => {
+            const mermaidCode = block.textContent;
+
+            const svg = mermaid.render(`mermaid-${index}`, mermaidCode);
+            const mermaidDiv = document.createElement('div');
+
+            // mermaid-js produces elements that the sanitizer does not recognize as safe,
+            // but it's all SVG content, so we can safely bypass the sanitizer here.
+            // eslint-disable-next-line powerbi-visuals/no-inner-outer-html
+            mermaidDiv.innerHTML = svg;
+
+            block.replaceWith(mermaidDiv);
+
+            // Add the mermaid-diagram class to the parent pre tag so we can center the
+            // diagram and remove background color.
+            const preTag = mermaidDiv.parentElement;
+            preTag.classList.add('mermaid-diagram');
+        });
     }
 
     /**
